aab328c651c51dcf031671ee881991f21fd366f8
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-07-16 12:41:22 +0900

    webpackerのcompileの実行、フロントエンドのみで「Hello Vue!」を表示させる。

--- babel.config.js
+++ babel.config.js
@@ -45,6 +45,10 @@ module.exports = function(api) {
         '@babel/plugin-proposal-class-properties',
         {
           loose: true
+        },
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
         }
       ],
       [
