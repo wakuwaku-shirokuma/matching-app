77e0e1456b4071274f74089596893649723ca5f7
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-07-17 23:34:35 +0900

    「app.vue」をテーブル表示に

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,41 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <table>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>name</th>
+          <th>birth</th>
+          <th>gender</th>
+          <th>joined_date</th>
+          <th>note</th>
+        </tr>
+        <tr v-for="e in users" :key="e.id">
+          <td>{{ e.id }}</td>
+          <td>{{ e.name }}</td>
+          <td>{{ e.birth }}</td>
+          <td>{{ e.gender }}</td>
+          <td>{{ e.joined_date }}</td>
+          <td>{{ e.note }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      users: []
     }
+  },
+  mounted () {
+    axios
+      .get('/api/v1/users.json')
+      .then(response => (this.users = response.data))
   }
 }
 </script>
