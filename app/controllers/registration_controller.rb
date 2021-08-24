661ccf8c6cfb6a952cfb215f9cbee6d997821d86
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-24 11:01:16 +0900

    登録・登録確認フォーム　htmlのテンプレート化

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,10 +1,14 @@
 class RegistrationController < ApplicationController
   
   def entry
+    @title = "つながるための準備、してみよう！"
+    @h1 = "つながるための準備、してみよう！"
     #@user = User.new
   end
 
   def confirm
+    @title = "つながるための準備、できた？"
+    @h1 = "つながるための準備、できた？"
     @user = User.new(user_params)
     #render action: "entry"
   end  
