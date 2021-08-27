65066f60a1582ae62add1092f30ad37b20dc2920
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-27 13:22:19 +0900

    【ユーザー側画面】左サイドバーの項目に応じて右メインコンテンツの内容の出しわけを実装

--- frontend/src/javascripts/components/vueUser.vue
+++ frontend/src/javascripts/components/vueUser.vue
@@ -16,13 +16,20 @@
       </v-sheet>
       <v-divider></v-divider>
       <v-list>
-        <v-list-item v-for="[icon, text] in links" :key="icon" link>
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
+            <v-list-item-title @click="isSelect('1')">お相手一覧</v-list-item-title>
           </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title @click="isSelect('2')">お役立ちコラム</v-list-item-title>
+          </v-list-item-content>    
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title @click="isSelect('3')">現在のつながり</v-list-item-title>
+          </v-list-item-content> 
         </v-list-item>
       </v-list>
       <!-- usersに格納された配列を元に表示する -->
@@ -31,31 +38,49 @@
       <vue-user-item v-for="user in users" v-bind:key="user.id" v-bind:name="user.name" v-bind:id="user.id"></vue-user-item>
     </v-navigation-drawer>
 
-    <v-main>
-      <v-container class="py-8 px-6" fluid>
-        <v-row>
-          <v-col v-for="card in cards" :key="card" cols="12">
-            <v-card>
-              <v-subheader>{{ card }}</v-subheader>
-              <v-list two-line>
-                <template v-for="n in 6">
-                  <v-list-item :key="n">
-                    <v-list-item-avatar color="grey darken-1"></v-list-item-avatar>
-                    <v-list-item-content>
-                      <v-list-item-title>Message {{ n }}</v-list-item-title>
-                      <v-list-item-subtitle>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
-                      </v-list-item-subtitle>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-divider v-if="n !== 6" :key="`divider-${n}`" inset></v-divider>
-                </template>
-              </v-list>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+    <div v-if="isActive === '1'">
+      <v-main>
+        <v-container class="py-8 px-6" fluid>
+          <v-row>
+            <v-col v-for="card in cards" :key="card" cols="12">
+              <v-card>
+                <v-subheader>{{ card }}</v-subheader>
+                <v-list two-line>
+                  <template v-for="n in 6">
+                    <v-list-item :key="n">
+                      <v-list-item-avatar color="grey darken-1"></v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-list-item-title>Message {{ n }}</v-list-item-title>
+                        <v-list-item-subtitle>
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                        </v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-divider v-if="n !== 6" :key="`divider-${n}`" inset></v-divider>
+                  </template>
+                </v-list>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </div>  
+
+    <div v-else-if="isActive === '2'">
+      <v-main>
+        <v-container class="py-8 px-6" fluid>
+          その２
+        </v-container>
+      </v-main>  
+    </div>
+
+    <div v-else-if="isActive === '3'">
+      <v-main>
+        <v-container class="py-8 px-6" fluid>
+          その３
+        </v-container>
+      </v-main>
+    </div>
 
   </v-app>
 </template>
@@ -75,12 +100,17 @@ export default {
       message: 'Message',
       cards: ['Today', 'Yesterday'],
       drawer: null,
-      links: [
-        ['mdi-inbox-arrow-down', 'Inbox'],
-        ['mdi-send', 'Send'],
-        ['mdi-delete', 'Trash'],
-        ['mdi-alert-octagon', 'Spam'],
-      ]
+      isActive: "1",
+      //links: [
+      //  ['1', 'その１'],
+      //  ['2', 'その２'],
+      //  ['3', 'その３'],
+      //]
+    }
+  },
+  methods: {
+    isSelect: function (num) {
+      this.isActive = num;
     }
   },
   //子コンポーネントを指定する
