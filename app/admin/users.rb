9a813923eb76976f9d5a01d4c2201cf8f32d0a55
wakuwaku-shirokuma <wakuwaku.shirokuma.works@gmail.com>
2021-12-19 14:20:03 +0900

    activeadminを一旦削除する

--- app/admin/users.rb
+++
@@ -1,3 +0,0 @@
-ActiveAdmin.register User do
-  permit_params :name, :gender, :birth, :joined_date, :note
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #ActiveAdmin.routes(self)
-  #root to: 'home#index'
-  
   # APIコントローラへのルーティング
   #namespace :api, {format: 'json'} do
   #  namespace :v1 do
